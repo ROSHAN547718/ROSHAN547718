b8ebc241bdeda0b7f9b2a04880990f56322b2b5d
ROSHAN547718 <ry4422951@gmail.com>
2025-06-30 15:12:28 +0530

    Day 15 - Java Functions

---
+++ Conditions.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Conditions {
+    private static final char[] April = null;
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // int sub = a-b;
+        // int mul = a*b;
+        // int divide = a/b;
+        // int modulus = a%b;
+        switch(a){
+            case 1 : char[] b = january;
+                System.out.println(b);
+            break;
+            case 2 : char[] february = null;
+                System.out.println(february);
+            break;
+            case 3 : char[] march = null;
+                System.out.println(march);
+            break;
+            case 4 : System.out.println(April);
+            break;
+            case 5 : char[] may = null;
+                System.out.println(may);
+            break;
+
+        }
+        
+        
+        
+    }
+}
